0372fceb937f6867bc1136b11cc282187a1e94fa
Camolas <manuelcurral@gmail.com>
2017-11-12 05:50:00 +0000

    Interface com altura vísivel das células

--- Megateh_Game/src/board&pieces.pl
+++ Megateh_Game/src/board&pieces.pl
@@ -42,29 +42,59 @@ empty_numPieces([[0,0,0,0],
                 ]).
 
 display_board([],_):-
-        write('     '), write('  ---------------------------------'), nl, nl,
-        write('     '), write('      A       B       C       D '), nl.
+        write('        '), put37code(220), nl, nl,
+        write('     '), write('      A         B         C         D '), nl.
+
+put2code(Code):-
+    put_code(Code),put_code(Code).
+
+put4code(Code):-
+    put2code(Code),put2code(Code).
+
+put8code(Code):-
+    put4code(Code),put4code(Code).   
+
+put16code(Code):-
+    put8code(Code),put8code(Code).
+
+put32code(Code):-
+    put16code(Code),put16code(Code).
+            
+put37code(Code):-
+        put32code(Code),put4code(Code),put2code(Code),put_code(Code).
 
 display_board([Head|Tail],NumRows):- 
         write('    '), write('   ---------------------------------'),nl,
-        write('    '), draw_empty_line([]),
-        write('   '), write(NumRows), draw_line(Head), nl,
-        write('    '), draw_empty_line([]),
+        write('        '),put37code(220),nl,
+        write('    '), draw_empty_line([]), 
+        write('  '), write(NumRows), draw_line(Head), nl,
+        write('    '), draw_number_line(Head),
         NewNumRows is NumRows - 1,
         display_board(Tail, NewNumRows).
 
 draw_empty_line([]):-
-        write('   '), write('|'),write('       '),
-        write('|'), write('       '), write('|'),
-        write('       '), write('|'), write('       '),
+        write('   '), write('|'),write('        '),  
+        write(' |'),write('        '), write(' |'),
+        write('         '), write('|'), write('         '),
         write('|'), nl.
 
+
+draw_number_line([]) :-
+        write('   |    '),
+        nl.
+
+draw_number_line([Head|Tail]):-
+        get_piece_height(Head, Level), 
+        write('   |     '), write(Level),
+        draw_number_line(Tail).
+
+
 draw_line([]) :-
-        write('   |   ').
+        write('    |    ').
 
 draw_line([Head|Tail]) :-
         char(Head,S),
-        write('   |   '), write(S), 
+        write('    |    '), write(S), 
         draw_line(Tail).
 
 get_piece_type(Char, Meaning):- char(Meaning,Char).
